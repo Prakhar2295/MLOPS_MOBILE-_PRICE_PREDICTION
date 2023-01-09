22abfa1c13b063933b1a162bf10c31886edb7474
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-01-09 23:11:48 +0530

    all_utils updated & .gitignore file updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -3,7 +3,14 @@ import os
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
-        content = yaml.safe_load(yaml_file):
+        content = yaml.safe_load(yaml_file)
 
 
-    return content    
+    return content
+
+
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok = True)
+        print(f"directory has been created at {dir_path}")
