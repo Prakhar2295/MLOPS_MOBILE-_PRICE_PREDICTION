26fb64a93e89ad18cfe1059ad6d92b60398966fc
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-01-10 23:12:52 +0530

    all_utils updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import pandas as pd
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
@@ -14,3 +15,9 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path,exist_ok = True)
         print(f"directory has been created at {dir_path}")
+
+
+def save_local_df(data, data_path):
+    data= data.to_csv(data_path, sep=",", index = False)
+    print(f"training data & testing data has been saved at {data_path}")
+    return data
